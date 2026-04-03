d2b618d0885adca26889e38ed96c774ffc8b5862
Davide Palese <davidepalese24@gmail.com>
2026-04-03 20:04:00 +0200

    App.tsx upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import UtentiList from './pages/UtentiList';
 import UtenteCreate from './pages/UtenteCreate';
 import UtenteDetail from './pages/UtenteDetail';
-import UtenteEdit from './pages/UtenteEdit'; // Importa la nuova pagina
+import UtenteEdit from './pages/UtenteEdit'; 
 import EmailTest from './pages/EmailTest';
 
 function App() {
@@ -19,7 +19,7 @@ function App() {
           <Route path="/utenti" element={<UtentiList />} />
           <Route path="/utenti/create" element={<UtenteCreate />} />
           <Route path="/utenti/:id" element={<UtenteDetail />} />
-          <Route path="/utenti/:id/edit" element={<UtenteEdit />} /> {/* Nuova rotta */}
+          <Route path="/utenti/:id/edit" element={<UtenteEdit />} /> 
           <Route path="/email-test" element={<EmailTest />} />
         </Routes>
       </Layout>
